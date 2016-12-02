2b31ae4adbf0a498f8f785e6ba66044d978c3c71
Anton Dolzhenko <anton.dolzhenko@onix-systems.com>
2016-12-02 11:31:02 +0200

    Add app entry point

--- OnixFirebaseChat/OnixFirebaseChat/Codebase/Common/AppDelegate.swift
+++ OnixFirebaseChat/OnixFirebaseChat/Codebase/Common/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var app: ONXFirebaseChat?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if let window = window {
+            app = ONXFirebaseChat(window: window)
+        }
         return true
     }
 
---
+++ OnixFirebaseChat/OnixFirebaseChat/Codebase/Common/ONXFirebaseChat.swift
@@ -0,0 +1,20 @@
+//
+//  OnixFirebaseChat.swift
+//  OnixFirebaseChat
+//
+//  Created by Anton Dolzhenko on 02.12.16.
+//  Copyright © 2016 Onix-systems. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+final class ONXFirebaseChat {
+
+    init(window: UIWindow) {
+        configure()
+    }
+    
+    private func configure(){
+    }
+}
