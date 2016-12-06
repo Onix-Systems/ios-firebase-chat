dc2c8f927405557ee08f7c2fedb6af6a8c26b5f8
Anton Dolzhenko <anton.dolzhenko@onix-systems.com>
2016-12-06 12:33:23 +0200

    Add SwiftLint close #7

--- OnixFirebaseChat/OnixFirebaseChat/Codebase/Common/Helpers/ONXAlertRenderer.swift
+++ OnixFirebaseChat/OnixFirebaseChat/Codebase/Common/Helpers/ONXAlertRenderer.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 protocol ONXAlertRenderer {
-    func displayMessage(_ title:String,msg:String)
-    func displayError(_ error:NSError)
+    func displayMessage(_ title: String, msg: String)
+    func displayError(_ error: NSError)
 }
 
 extension ONXAlertRenderer where Self: UIViewController {
-    func displayError(_ error:NSError) {
+    func displayError(_ error: NSError) {
         displayMessage("Error!", msg: error.localizedDescription)
     }
-    
-    func displayMessage(_ title:String,msg:String) {
+
+    func displayMessage(_ title: String, msg: String) {
         let alertController = UIAlertController(title: title, message: msg, preferredStyle: UIAlertControllerStyle.alert)
         let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel) { (action) -> Void in
             alertController.dismiss(animated: true, completion:nil)
